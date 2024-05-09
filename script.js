6915966d8ada9c6a602e59088829e39679b9a58b
Babyhex7 <113740394+Babyhex7@users.noreply.github.com>
2024-05-09 08:36:47 +0700

    Memperbagus tampilan page dan menambahkan transisi footer ( bagas )

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("toggleFooter").addEventListener("click", function () {
+    const footer = document.getElementById("myFooter");
+    const toggleButton = document.getElementById("toggleFooter");
+    
+    if (footer.style.height === "0px" || !footer.style.height) {
+        footer.style.height = "40px"; 
+        footer.style.opacity = "1"; 
+        toggleButton.textContent = "Sembunyikan"; 
+    } else {
+        footer.style.height = "0";
+        footer.style.opacity = "0"; 
+        toggleButton.textContent = "Tampilkan Referensi"; 
+    }
+});
+
